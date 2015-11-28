deac8c1c764221643f156c1c5f31642079a52b1f
Hughlaurie <Gorshkov.web@gmail.com>
2015-11-28 18:52:19 +0300

    Simple.js

---
+++ Simple.js
@@ -0,0 +1,13 @@
+var startPos = prompt("Введите число - начало диапазона!");
+var endPos = prompt("Введите число - конец диапазона!");
+var count = 0; 
+for (var i = endPos; i > 0; i--) 
+{
+	for (var j = startPos ; j <= endPos ; j++) 
+	{
+		if (i % j == 0) {count = count + 1};
+	}
+	var	check=count;
+	count = 0;
+	if (check == 2) alert(i);
+}
